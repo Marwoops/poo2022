6b8804c26b16dbf04d231e8b95645bca4c15bcc7
azizim <marwan.azizi@etu.u-paris.fr>
2022-11-15 16:28:26 +0100

    event souris / tuile

--- src/ui/vues/VuePlateau.java
+++ src/ui/vues/VuePlateau.java
@@ -7,20 +7,21 @@ public class VuePlateau extends JPanel {
     private Plateau plateau;
     private int hauteur;
     private int largeur;
+	private VueTuile[][] vues;
 
     public VuePlateau(int ha, int la, Plateau p) {
         hauteur = ha;
         largeur = la;
-
+		vues = new VueTuile[ha][la];
         plateau = p;
         
         setLayout(null);
 
         for (int i = 0; i < p.getHauteur(); i++) {
             for (int j = 0; j < p.getLargeur(); j++) {
-                JLabel t = new VueTuile(p.getTuile(i+1, j+1));
-                t.setBounds(i*80, j*80, 80, 80);
-                add(t);
+                vues[i][j] = new VueTuile(p.getTuile(i+1, j+1));
+                vues[i][j].setBounds(i*80, j*80, 80, 80);
+                add(vues[i][j]);
             }
         }
     }
--- src/ui/vues/VueTuile.java
+++ src/ui/vues/VueTuile.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.awt.geom.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.io.File;
 import javax.imageio.ImageIO;
@@ -12,11 +13,12 @@ public class VueTuile extends JLabel {
 
     private Tuile tuile;
     private BufferedImage icone;
+	private boolean brillance;
     
     public static BufferedImage genererIcone(BufferedImage img, int i) {
         int largeur = img.getWidth();
         int hauteur = img.getHeight();
- 
+
         BufferedImage nouvelleIcone = new BufferedImage(largeur, hauteur, img.getType());
  
         Graphics2D g2 = nouvelleIcone.createGraphics();
@@ -29,7 +31,33 @@ public class VueTuile extends JLabel {
 
     public VueTuile(Tuile t) {
         tuile = t;
-        
+		brillance = false;
+		
+		addMouseListener(new MouseListener() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						System.out.println("meurs");
+					}
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						brillance = true;
+						repaint();
+					}
+					@Override
+					public void mouseExited(MouseEvent e) {
+						brillance = false;
+						repaint();
+					}
+					@Override
+					public void mouseReleased(MouseEvent e) {
+
+					}
+					@Override
+					public void mousePressed(MouseEvent e) {
+
+					}
+				});
+
         if (t != null) {
             try {
                 BufferedImage img = ImageIO.read(new File(cheminIcone + ((Parcelle) tuile).getId() + ".png"));
@@ -42,8 +70,12 @@ public class VueTuile extends JLabel {
         }
     }
 
+	public void setBrillance(boolean b) {
+		brillance = b;
+	}
+
     public void paintComponent(Graphics g) {
-        if (icone != null)
+        if (icone != null && !brillance)
             g.drawImage(icone, 0, 0, 80, 80, null); // 80 correspond à la taille du côté (en px) de la tuile dans le plateau
         else
             g.drawString("0", 40, 40);
