72609d542c0dde790f83dadb435b6e138d124084
S1nj <marwan.azizi@gmail.com>
2022-12-18 12:02:51 +0100

    construction des 84 domino

--- src/domino/Rangee.java
+++ src/domino/Rangee.java
@@ -6,8 +6,13 @@ public class Rangee extends Cote {
         points = p;
     }
 
-    // constructeur rangee aleatoire ?
-    
+	public Rangee(int a, int b, int c) {
+		points = new int[3];
+		points[0] = a;
+		points[1] = b;
+		points[2] = c;
+	}
+
     public int getPoint(int i) {
         return points[i];
     }
