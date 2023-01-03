8a2fc40911b74da18d84bb37ed8383e613be01d6
S1nj <marwan.azizi@gmail.com>
2023-01-03 23:11:29 +0100

    l'ia joue après avoir défausser dans carca

--- src/ui/vues/VuePartieDeCarcassonne.java
+++ src/ui/vues/VuePartieDeCarcassonne.java
@@ -118,6 +118,7 @@ public class VuePartieDeCarcassonne extends VuePartie {
 			super.postDefausse();
 			partie.getJoueurCourant().pioche();
 			pioche();
+			preTour(courant);
 		}
 
 		public void postPose(VueTuile v) {
