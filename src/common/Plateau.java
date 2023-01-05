b129ff443a402904ee21a3a1c22bfae786526643
S1nj <marwan.azizi@gmail.com>
2023-01-05 10:46:27 +0100

    séparation de estPosable en 2 fonctions

--- src/common/Plateau.java
+++ src/common/Plateau.java
@@ -28,15 +28,20 @@ public class Plateau {
 		return plat;
 	}
 
-	public boolean estPosable(int x, int y, Tuile t) {
+	public boolean estSelectionnable(int x, int y) {
 		if (horsLimite(x,y) || getTuile(x,y) != null) return false;
 		Tuile haut = getTuile(x-1, y);
 		Tuile droite = getTuile(x, y+1);
 		Tuile bas = getTuile(x+1, y);
 		Tuile gauche = getTuile(x, y-1);
 
-		return (haut != null || droite != null || bas != null || gauche != null) && t.estCompatible(0, haut) && t.estCompatible(1, droite)
-			&& t.estCompatible(2, bas) && t.estCompatible(3, gauche);
+		return (haut != null || droite != null || bas != null || gauche != null);
+	}
+
+	public boolean estPosable(int x, int y, Tuile t) {
+		return (estSelectionnable(x, y)
+			&& t.estCompatible(0, getTuile(x-1, y)) && t.estCompatible(1, getTuile(x, y+1))
+			&& t.estCompatible(2, getTuile(x+1, y)) && t.estCompatible(3, getTuile(x, y-1)));
 	}
 
 	public void setTuile(int x, int y, Tuile t) {
